9d936c2e81cc43ab2e0e6fd9f411c56dbc872d1d
phantomramen <ali.hernandez9two@gmail.com>
2021-07-22 21:35:27 -0600

    pics n fun stuff

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import fourthDim from '../../images/fourthDimStore.jpeg';
+import milivy from '../../images/milesivy.jpeg';
 import './Home.css';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <h2>About:</h2>
             <p>Busey ipsum dolor sit amet. You gotta go through it to see there ain't nothing to it.Go with the feeling of the nature. Take it easy. Know why you're here. And remember to balance your internal energy with the environment.You ever roasted doughnuts?The best way to communicate is compatible. Compatible communication is listening with open ears and an open mind, and not being fearful or judgemental about what you're hearing.
 </p>
+            <img src={ milivy } alt="mils" className="owners" />
             <h2>Contact:</h2>
             <p>contact info here</p>
 
